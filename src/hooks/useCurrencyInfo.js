e7a6261db56aa58d3dda022a0575162be44bdf9f
RitankJaikar <ritank.jaikar@gmail.com>
2024-09-17 21:51:05 +0530

    error handling

--- src/hooks/useCurrencyInfo.js
+++ src/hooks/useCurrencyInfo.js
@@ -7,7 +7,8 @@ function useCurrencyInfo(currency) {
     useEffect(() => {
         fetch(`https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${currency}.json`)
         .then(res => res.json())
-        .then(res => setData(res[currency]));
+        .then(res => setData(res[currency]))
+        .catch(err => console.error(err));
     }, [currency]);
     console.log(data);
     return data;
